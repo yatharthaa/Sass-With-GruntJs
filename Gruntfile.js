bb769075190071246e17bb66d626203cc108f3f3
sandip bhandari <sandipp.bhandari@gmail.com>
2020-11-07 21:29:24 +0545

    sass with grunt js

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: false,
+                    compress: false,
+                    yuicompress: false,
+                    style: 'expanded',
+                },
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                }
+            },
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['watch']);
+}
